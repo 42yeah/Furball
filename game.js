680a5d2c5614a52a9f5c31e60aaf956f74725cc9
42yeah <potion@live.cn>
2019-03-31 16:52:13 +0800

    first commit

--- game.js
+++ game.js
@@ -3,14 +3,18 @@
 const gameCanvas = document.getElementById("game");
 
 const logger = document.getElementById("logger");
-var state = 0;
-var texSize = {
+let state = 0;
+let texSize = {
   width: 50,
   height: 50
 };
-var screenSize = {
+let screenSize = {
   width: 500,
   height: 500
+};
+let levelSize = {
+  width: 10,
+  height: 8
 };
 const ctx = gameCanvas.getContext("2d");
 
@@ -21,20 +25,24 @@ const spike = new Image();
 const brook = new Image();
 const huh = new Image();
 const dead = new Image();
+const whirl = new Image();
 
-var stuffs = [];
-var levels = [];
-var curLvl = 0;
-var bond = null;
-var totalCarrots = 0;
-var levelCarrots = 0;
+let stuffs = [];
+let levels = [];
+let curLvl = 0;
+let bond = null;
+let totalCarrots = 0;
+let levelCarrots = 0;
+let crashed = null;
+let line = null;
+let dPos = null;
 
 function log(what) {
   logger.value += "\n" + what;
 }
 
 function isReady() {
-  return (state >= 8);
+  return (state >= 9);
 }
 
 function initLevel(advance) {
@@ -45,21 +53,24 @@ function initLevel(advance) {
   stuffs = [];
   bond = null;
   levelCarrots = 0;
+  line = null;
+  dPos = null;
+  crashed = null;
   
-  var lvl = levels[curLvl];
-  stuffs.push(stuff(furball, "furball", lvl.furball.x, lvl.furball.y));
-  stuffs[0].alive = true;
-  stuffs[0].carrots = 0;
+  let lvl = levels[curLvl];
+  let mainFurball = stuff(furball, "furball", lvl.furball.x, lvl.furball.y);
+  mainFurball.alive = true;
+  mainFurball.carrots = totalCarrots;
       
-  for (var i = 0; i < lvl.carrots.length; i++) {
+  for (let i = 0; i < lvl.carrots.length; i++) {
     stuffs.push(stuff(carrot, "carrot", lvl.carrots[i].x, lvl.carrots[i].y));
   }
   
   if (!("terrains" in lvl)) {
     return;
   }
-  for (var i = 0; i < lvl.terrains.length; i++) {
-    var sprite;
+  for (let i = 0; i < lvl.terrains.length; i++) {
+    let sprite;
     switch (lvl.terrains[i].name) {
       case "spike":
         sprite = spike;
@@ -68,12 +79,18 @@ function initLevel(advance) {
       case "brook":
         sprite = brook;
         break;
+
+      case "whirl":
+        sprite = whirl;
+        break;
       
       default:
         sprite = huh;
     }
     stuffs.push(stuff(sprite, lvl.terrains[i].name, lvl.terrains[i].x, lvl.terrains[i].y));
   }
+
+  stuffs.push(mainFurball);
 }
 
 function seeIfReadyToGo() {
@@ -94,14 +111,74 @@ function seeIfReadyToGo() {
 
 function loadLvls() {
   // shit, write them in code first...
-  fetch("levels.json").then(function(res) {
-    return res.json();
-  }).then(function(json) {
-    levels = json;
+  fetch("levels.txt").then(function(res) {
+    res.body.getReader().read().then(function (content) {
+      let str = new TextDecoder("utf-8").decode(content.value);
+      let gotLines = str.split("\n");
+      let numLevels = Math.floor(gotLines.length / 9);
+
+      for (let i = 0; i < numLevels; i++) {
+        let story = gotLines[i * 9 + 8];
+        let furball = null;
+        let carrots = [];
+        let terrains = [];
+        for (let y = 0; y < levelSize.height; y++) {
+          for (let x = 0; x < gotLines[i * 9 + y].length; x++) {
+            switch (gotLines[i * 9 + y][x]) {
+              case 'o':
+                furball = {
+                  x: x,
+                  y: y,
+                  name: "furball"
+                };
+                break;
+
+              case '!':
+                carrots.push({
+                  x: x,
+                  y: y,
+                  name: "carrot"
+                });
+                break;
+
+              case '~':
+                terrains.push({
+                  x: x,
+                  y: y,
+                  name: "brook"
+                });
+                break;
+
+              case '^':
+                terrains.push({
+                  x: x,
+                  y: y,
+                  name: "spike"
+                });
+                break;
+
+              case '+':
+                terrains.push({
+                  x: x,
+                  y: y,
+                  name: "whirl"
+                });
+                break;
+            }
+          }
+        }
+        levels.push({
+          furball: furball,
+          carrots: carrots,
+          terrains: terrains,
+          story: story,
+        });
+      }
+      seeIfReadyToGo();
+    });
   }).catch(function(e) {
     log("ERR: " + e);
   });
-  seeIfReadyToGo();
 }
 
 function reallify(fakeX, fakeY) {
@@ -133,40 +210,73 @@ function stuff(sprite, name, x, y) {
 }
 
 function isCrashing() {
-  for (var i = 0; i < stuffs.length; i++) {
+  let ret = null;
+  for (let i = 0; i < stuffs.length; i++) {
     if (stuffs[i].name != "furball") { continue; }
     else {
-      var sheep = stuffs[i];
+      let sheep = stuffs[i];
       if (!sheep.alive) { continue; }
-      for (var j = 0; j < stuffs.length; j++) {
-        if (stuffs[j].name == "furball") { continue; }
-        var now = stuffs[j];
+      for (let j = 0; j < stuffs.length; j++) {
+        if (stuffs[j].name == "furball" && stuffs[j].alive) { continue; }
+        let now = stuffs[j];
+        
         if (!((sheep.x + 0.5 < now.x) ||
           (now.x + 0.5 < sheep.x) ||
           (sheep.y + 0.5 < now.y) ||
           (now.y + 0.5 < sheep.y))) {
-            return {
-              who: sheep,
-              what: now
-            };
+          if (now.name == "spike" || now.name == "brook" || now.name == "whirl") {
+            let kill = true;
+            if (sheep.ax || sheep.ay) {
+              if ((Math.abs(sheep.ax) >= 0.1 || Math.abs(sheep.ay) >= 0.1) && now.name != "whirl") {
+                kill = false;
+              }
+            }
+            
+            if (kill) {
+              // kill that sheep!
+              sheep.alive = false;
+              sheep.sprite = dead;
+              bond = null;
+              
+              for (let i = 0; i < sheep.carrots; i++) {
+                // generate some carrots
+                let legacy = stuff(carrot, "carrot", sheep.x, sheep.y);
+                let sign = Math.random() > 0.5 ? 1 : -1;
+                let sign2 = Math.random() > 0.5 ? 1 : -1;
+                legacy.ax = sign * Math.random() * 1.0;
+                legacy.ay = sign2 * Math.random() * 1.0;
+                stuffs.push(legacy);
+              }
+              continue;
+            }
+          }
+          if (ret && ret.what.name == "furball" && now.name != "carrot") {
+            // nope, firstify furball
+            continue;
+          }
+          ret = {
+            who: sheep,
+            what: now
+          };
+          if (ret.what.name == "carrot") { return ret; }
         }
       }
     }
   }
-  return null;
+  return ret;
 }
 
 function gameLoop() {
   requestAnimationFrame(gameLoop);
   ctx.fillRect(0, 0, screenSize.width, screenSize.height);
-  for (var y = 0; y < 8; y++) {
-    for (var x = 0; x < 10; x++) {
+  for (let y = 0; y < levelSize.height; y++) {
+    for (let x = 0; x < levelSize.width; x++) {
       draw(grass, x, y);
     }
   }
   
-  var metCarrots = false;
-  for (var i = 0; i < stuffs.length; i++) {
+  let metCarrots = false;
+  for (let i = 0; i < stuffs.length; i++) {
     const thing = stuffs[i];
     draw(thing.sprite, thing.x, thing.y);
     if (thing.name == "carrot") {
@@ -174,56 +284,53 @@ function gameLoop() {
     }
     
     if ("ax" in thing) {
-      var dx = thing.ax / 5.0;
+      let dx = thing.ax / 5.0;
       thing.ax -= dx;
       thing.x += dx;
     }
     if ("ay" in thing) {
-      var dy = thing.ay / 5.0;
+      let dy = thing.ay / 5.0;
       thing.ay -= dy;
       thing.y += dy;
     }
   }
   ctx.fillStyle = "#fff";
   ctx.font = "20px sans-serif";
-  var textPos = {
+  let textPos = {
     x: 20,
     y: 430
   };
-  var lines = levels[curLvl].story.split("\n");
-  for (var i = 0; i < lines.length; i++) {
+  let lines = levels[curLvl].story.split("\\n");
+  for (let i = 0; i < lines.length; i++) {
     ctx.fillText(lines[i], textPos.x, textPos.y);
     textPos.y += 24;
   }
   ctx.fillStyle = "#000";
   
-  var crashed = isCrashing();
+  if (line) {
+    ctx.beginPath();
+    ctx.moveTo(line[0].x, line[0].y);
+    for (let i = 1; i < line.length; i++) {
+      ctx.lineTo(line[i].x, line[i].y);
+    }
+    ctx.stroke();
+  }
+  
+  crashed = isCrashing();
   if (crashed) {
     if (crashed.what.name == "carrot") {
       // chomp chomp
-      var index = stuffs.indexOf(crashed.what);
+      let index = stuffs.indexOf(crashed.what);
       stuffs.splice(index, 1);
       totalCarrots++;
       levelCarrots++;
       crashed.who.carrots++;
     } else if (crashed.what.name == "spike" || crashed.what.name == "brook") {
-      // kill that sheep!
-      crashed.who.alive = false;
-      crashed.who.sprite = dead;
-      bond = null;
       
-      for (var i = 0; i < crashed.who.carrots; i++) {
-        // generate some carrots
-        var legacy = stuff(carrot, "carrot", crashed.who.x, crashed.who.y);
-        var sign = Math.random() > 0.5 ? 1 : -1;
-        legacy.ax = sign * Math.random() * 1.0;
-        legacy.ay = sign * Math.random() * 1.0;
-        stuffs.push(legacy);
-      }
     }
     
     if (levelCarrots % 2 == 0 && levelCarrots != 0) {
-      var newFurball = stuff(crashed.who.sprite, crashed.who.name, crashed.who.x, crashed.who.y);
+      let newFurball = stuff(crashed.who.sprite, crashed.who.name, crashed.who.x, crashed.who.y);
       newFurball.carrots = 0;
       newFurball.alive = true;
       stuffs.push(newFurball);
@@ -237,24 +344,29 @@ function gameLoop() {
 }
 
 function getMousePos(canvasDom, touch) {
-  var rect = canvasDom.getBoundingClientRect();
+  let rect = canvasDom.getBoundingClientRect();
   return { x: touch.clientX - rect.left, y: touch.clientY - rect.top };
 }
 
 gameCanvas.ontouchstart = function(e) {
-  var touch = e.touches[0];
-  var realPos = getMousePos(gameCanvas, touch);
-  var fakePos = tilify(realPos.x, realPos.y);
+  let touch = e.touches[0];
+  let realPos = getMousePos(gameCanvas, touch);
+  let fakePos = tilify(realPos.x, realPos.y);
+  bond = null;
   
   // 0 is always the sheep
-  for (var i = 0; i < stuffs.length; i++) {
+  for (let i = 0; i < stuffs.length; i++) {
     if (stuffs[i].name == "furball") {
-      var sheep = stuffs[i];
+      let sheep = stuffs[i];
       if (!sheep.alive) { continue; }
-      if (!((fakePos.x + 1.0 < sheep.x) ||
+      if (!((fakePos.x + 0.01 < sheep.x) ||
         (sheep.x + 1.0 < fakePos.x) ||
-        (fakePos.y + 1.0 < sheep.y) ||
+        (fakePos.y + 0.01 < sheep.y) ||
         (sheep.y + 1.0 < fakePos.y))) {
+          dPos = {
+            x: sheep.x - fakePos.x,
+            y: sheep.y - fakePos.y
+          };
           bond = stuffs[i];
       }
     }
@@ -262,16 +374,45 @@ gameCanvas.ontouchstart = function(e) {
 }
 
 gameCanvas.ontouchend = function(e) {
+  let touch = e.touches[0];
+
+  if (line && bond) {
+    let realPos = line[0];
+    let fakePos = tilify(realPos.x, realPos.y);
+
+    let dx = fakePos.x - bond.x;
+    let dy = fakePos.y - bond.y;
+    bond.ax = -dx / 1.0;
+    bond.ay = -dy / 1.0;
+    line = null;
+  }
+
   bond = null;
+  dPos = null;
 };
 
 gameCanvas.ontouchmove = function(e) {
-  var touch = e.touches[0];
-  var realPos = getMousePos(gameCanvas, touch);
-  var fakePos = tilify(realPos.x, realPos.y);
+  let touch = e.touches[0];
+  let realPos = getMousePos(gameCanvas, touch);
+  let fakePos = tilify(realPos.x, realPos.y);
+  
   if (bond) {
-    bond.x = fakePos.x;
-    bond.y = fakePos.y;
+    if (crashed && crashed.who == bond && crashed.what.name == "furball") {
+      // crashed a dead furball!
+      
+      let furballRealPos = reallify(crashed.who.x, crashed.who.y);
+      furballRealPos.x -= texSize.width * dPos.x;
+      furballRealPos.y -= texSize.height * dPos.y;
+      line = [
+        realPos,
+        furballRealPos
+      ];
+      
+    } else {
+      line = null;
+      bond.x = fakePos.x + dPos.x;
+      bond.y = fakePos.y + dPos.y;
+    }
   }
   
   e.preventDefault();
@@ -323,6 +464,8 @@ huh.src = "huh.png";
 huh.addEventListener("load", seeIfReadyToGo);
 dead.src = "dead.png";
 dead.addEventListener("load", seeIfReadyToGo);
+whirl.src = "whirl.png";
+whirl.addEventListener("load", seeIfReadyToGo);
 
 log("Loading levels");
 loadLvls();
